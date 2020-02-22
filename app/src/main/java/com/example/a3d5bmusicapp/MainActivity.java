55dc56979fe466a42db77c1570ace099987efce2
juliegallagher <60260903+juliegallagher@users.noreply.github.com>
2020-02-22 21:37:58 +0000

    Login in page UI added

--- app/src/main/java/com/example/a3d5bmusicapp/MainActivity.java
+++ app/src/main/java/com/example/a3d5bmusicapp/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.a3d5bmusicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.loginbubble);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogInPage();
+            }
+        });
+
+
+
+    }
+
+    public void openLogInPage(){
+        Intent intent = new Intent(this, Log_In_page.class);
+        startActivity(intent);
+
     }
 }
