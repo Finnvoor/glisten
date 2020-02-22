dacac5ecc194eda96b002b841c4de34c0db71d73
lil137 <scyyh2@nottingham.edu.cn>
2020-02-22 15:52:13 +0000

    Now the user can login once they open the app and see their spotify's user name at the home page

--- app/src/main/java/com/example/a3d5bmusicapp/MainActivity.java
+++ app/src/main/java/com/example/a3d5bmusicapp/MainActivity.java
@@ -1,16 +1,53 @@
 package com.example.a3d5bmusicapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button guestbutton;
     private Button hostbutton;
+    private TextView mUser;
+
+
+    private static final String CLIENT_ID = "d7df40ed56214e28af69045f70adee3e";
+    private static final String REDIRECT_URI = "com.example.a3d5bmusicapp://callback";
+    private static final int REQUEST_CODE = 1337;
+    private static final String SCOPES = "user-read-recently-played,user-library-modify,user-read-email,user-read-private";
+
+    private SharedPreferences.Editor editor;
+    private SharedPreferences msharedPreferences;
+
+    private RequestQueue queue;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
 
         guestbutton = findViewById(R.id.btn_guest);
         hostbutton = findViewById(R.id.btn_host);
+        mUser = findViewById(R.id.tv_userinfo);
+
+        authenticateSpotify();
+        msharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        queue = Volley.newRequestQueue(this);
+        getUserInfor();
 
 
         hostbutton.setOnClickListener(new View.OnClickListener() {
@@ -36,5 +79,94 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void authenticateSpotify() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{SCOPES});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    editor = getSharedPreferences("SPOTIFY", 0).edit();
+                    editor.putString("token", response.getAccessToken());
+                    Log.d("STARTING", "GOT AUTH TOKEN");
+                    editor.apply();
+                    //waitForUserInfo();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    break;
+                // Handle other cases
+            }
+        }
+    }
+
+    private void getUserInfor() {
+        String endpoint = "https://api.spotify.com/v1/me";
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, endpoint,null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //Log.d("Response", response.toString());
+                        try {
+                            String user_name = response.getString("display_name");
+                            mUser.setText("Current User: " +user_name);
+                        }catch (JSONException e){
+                            Log.d("ERROR","error => "+e.toString());
+                        }
+
+                        //Log.d("Response", response.substring(1000, 2000));
+                        //Log.d("Response", response.substring(2000, 3000));
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR","error => "+error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer " + msharedPreferences.getString("token", ""));
+
+                return params;
+            }
+
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String>  params = new HashMap<String, String>();
+//                params.put("q", "rock");
+//                params.put("type", "track");
+//
+//                return params;
+//            }
+        };
+        queue.add(getRequest);
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/a3d5bmusicapp/VolleyCallBack.java
+++
@@ -1,6 +0,0 @@
-package com.example.a3d5bmusicapp;
-
-public interface VolleyCallBack {
-
-    void onSuccess();
-}
