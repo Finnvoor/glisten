2aa6330d14e03cc017c6e6ce8ad11b578eaa5020
lil137 <scyyh2@nottingham.edu.cn>
2020-02-24 23:29:30 +0000

    Now the user can research result page would show user "songs", "singers", "albums" these three category and user can switch between different pages by clicking the button at the top.

--- app/src/main/java/com/example/a3d5bmusicapp/SearchResultActivity.java
+++ app/src/main/java/com/example/a3d5bmusicapp/SearchResultActivity.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -42,10 +43,28 @@ public class SearchResultActivity extends AppCompatActivity {
     private TextView tv_sr_1;
     private TextView tv_sr_2;
 
+    private ImageView iv_singer_1;
+    private TextView sr_tv_singer_1;
+    private TextView sr_tv_singer_popu_1;
+
+    private ImageView iv_album_1;
+    private TextView sr_tv_album_title;
+    private TextView sr_tv_album_artist;
+
+
     private SharedPreferences.Editor editor;
     private SharedPreferences msharedPreferences;
 
     private RequestQueue queue;
+
+    private GridLayout grid_songs;
+    private GridLayout grid_singers;
+    private GridLayout grid_albums;
+
+    private Button option_songs;
+    private Button option_singers;
+    private Button option_albums;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +77,21 @@ public class SearchResultActivity extends AppCompatActivity {
         iv_1 = findViewById(R.id.iv_1);
         tv_sr_1 = findViewById(R.id.sr_tv_1);
         tv_sr_2 = findViewById(R.id.sr_tv_2);
+        grid_songs = findViewById(R.id.grid_songs);
+        grid_singers = findViewById(R.id.grid_singers);
+        grid_albums = findViewById(R.id.grid_albums);
+
+        iv_singer_1 =findViewById(R.id.iv_singer_1);
+        sr_tv_singer_1 = findViewById(R.id.sr_tv_singer_1);
+        sr_tv_singer_popu_1 = findViewById(R.id.sr_tv_singer_popu_1);
+
+        option_songs = findViewById(R.id.result_songs);
+        option_singers = findViewById(R.id.result_singers);
+        option_albums = findViewById(R.id.result_albums);
+
+        iv_album_1 = findViewById(R.id.iv_album_1);
+        sr_tv_album_title = findViewById(R.id.sr_tv_album_1);
+        sr_tv_album_artist = findViewById(R.id.sr_tv_album_2);
 
         btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +99,36 @@ public class SearchResultActivity extends AppCompatActivity {
                 //Intent searchpage_intent = new Intent(SearchResultActivity.this,SearchActivity.class);
                 //startActivity(searchpage_intent);
                 finish();
+            }
+        });
+
+        option_songs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grid_songs.setVisibility(View.VISIBLE);
+                grid_singers.setVisibility(View.GONE);
+                grid_albums.setVisibility(View.GONE);
+
+            }
+        });
+
+        option_singers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grid_songs.setVisibility(View.GONE);
+                grid_singers.setVisibility(View.VISIBLE);
+                grid_albums.setVisibility(View.GONE);
+
+            }
+        });
+
+        option_albums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grid_songs.setVisibility(View.GONE);
+                grid_singers.setVisibility(View.GONE);
+                grid_albums.setVisibility(View.VISIBLE);
+
             }
         });
 
@@ -80,6 +144,8 @@ public class SearchResultActivity extends AppCompatActivity {
 
         //displayResult(keyword);
         getSongs(keyword);
+        getSingers(keyword);
+        getAlbums(keyword);
 
 
     }
@@ -90,7 +156,7 @@ public class SearchResultActivity extends AppCompatActivity {
 
 
     private void getSongs(String keyword) {
-        String endpoint = "https://api.spotify.com/v1/search?query=" + keyword + "&offset=0&limit=20&type=track";
+        String endpoint = "https://api.spotify.com/v1/search?query=" + keyword + "&offset=0&limit=1&type=track";
         JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, endpoint,null,
                 new Response.Listener<JSONObject>()
                 {
@@ -110,9 +176,6 @@ public class SearchResultActivity extends AppCompatActivity {
 
                             setMarquee(tv_sr_1,name);
                             setMarquee(tv_sr_2,artist);
-
-
-
 
                         Glide.with(SearchResultActivity.this).load(cover).into(iv_1);
 
@@ -153,6 +216,131 @@ public class SearchResultActivity extends AppCompatActivity {
         queue.add(getRequest);
     }
 
+
+    private void getSingers(String keyword) {
+        String endpoint = "https://api.spotify.com/v1/search?query=" + keyword + "&offset=0&limit=1&type=artist";
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, endpoint,null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Response", response.toString());
+
+                        try {
+                            //String user_name = response.getString("display_name");
+                            //mUser.setText("Current User: " +user_name);
+
+
+                            String cover = response.getJSONObject("artists").getJSONArray("items").getJSONObject(0).getJSONArray("images").getJSONObject(2).getString("url");
+                            Integer popularity =response.getJSONObject("artists").getJSONArray("items").getJSONObject(0).getInt("popularity");
+
+                            String name = response.getJSONObject("artists").getJSONArray("items").getJSONObject(0).getString("name");
+
+                            //iv_singer_1 =findViewById(R.id.iv_singer_1);
+                            //sr_tv_singer_1 = findViewById(R.id.sr_tv_singer_1);
+                            sr_tv_singer_popu_1.setText("Popularity: " +popularity);
+
+                            setMarquee(sr_tv_singer_1,name);
+                            Glide.with(SearchResultActivity.this).load(cover).into(iv_singer_1);
+
+                        }catch (JSONException e){
+                            Log.d("ERROR","error => "+e.toString());
+                        }
+
+                        //Log.d("Response", response.substring(1000, 2000));
+                        //Log.d("Response", response.substring(2000, 3000));
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR","error => "+error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer " + msharedPreferences.getString("token", ""));
+
+                return params;
+            }
+
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String>  params = new HashMap<String, String>();
+//                params.put("q", "rock");
+//                params.put("type", "track");
+//
+//                return params;
+//            }
+        };
+        queue.add(getRequest);
+    }
+
+
+    private void getAlbums(String keyword) {
+        String endpoint = "https://api.spotify.com/v1/search?query=" + keyword + "&offset=0&limit=1&type=album";
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, endpoint,null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Response", response.toString());
+
+                        try {
+                            //String user_name = response.getString("display_name");
+                            //mUser.setText("Current User: " +user_name);
+
+                            String artist = response.getJSONObject("albums").getJSONArray("items").getJSONObject(0).getJSONArray("artists").getJSONObject(0).getString("name");
+                            String cover = response.getJSONObject("albums").getJSONArray("items").getJSONObject(0).getJSONArray("images").getJSONObject(2).getString("url");
+
+
+                            String title = response.getJSONObject("albums").getJSONArray("items").getJSONObject(0).getString("name");
+                            String release_date = response.getJSONObject("albums").getJSONArray("items").getJSONObject(0).getString("release_date");
+
+                            setMarquee(sr_tv_album_artist,artist);
+                            setMarquee(sr_tv_album_title,title + "(" + release_date + ")");
+
+                            Glide.with(SearchResultActivity.this).load(cover).into(iv_album_1);
+
+
+                        }catch (JSONException e){
+                            Log.d("ERROR","error => "+e.toString());
+                        }
+
+                        //Log.d("Response", response.substring(1000, 2000));
+                        //Log.d("Response", response.substring(2000, 3000));
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR","error => "+error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer " + msharedPreferences.getString("token", ""));
+
+                return params;
+            }
+
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String>  params = new HashMap<String, String>();
+//                params.put("q", "rock");
+//                params.put("type", "track");
+//
+//                return params;
+//            }
+        };
+        queue.add(getRequest);
+    }
 
     private void setMarquee(TextView v, String text){
 
