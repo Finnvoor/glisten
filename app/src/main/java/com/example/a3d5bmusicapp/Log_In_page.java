97175166d0cf5e1ab35f674d842cdea04a92ba41
juliegallagher <60260903+juliegallagher@users.noreply.github.com>
2020-02-24 15:12:14 +0000

    Start new session page

--- app/src/main/java/com/example/a3d5bmusicapp/Log_In_page.java
+++ app/src/main/java/com/example/a3d5bmusicapp/Log_In_page.java
@@ -2,13 +2,33 @@ package com.example.a3d5bmusicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Log_In_page extends AppCompatActivity {
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log__in_page);
+
+        button2 = (Button) findViewById(R.id.LOGIN);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewSessionPage();
+            }
+        });
     }
+
+
+    public void openNewSessionPage() {
+        Intent intent = new Intent(this, new_session.class);
+        startActivity(intent);
+
+    }
+
 }
