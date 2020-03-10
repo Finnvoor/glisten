6598be197582503ddc783b3f1445016e74174bd1
lil137 <scyyh2@nottingham.edu.cn>
2020-03-10 15:32:02 +0000

    Add the time in seconds under the progress bar

--- app/src/main/java/com/example/a3d5bmusicapp/CurrentSongActivity.java
+++ app/src/main/java/com/example/a3d5bmusicapp/CurrentSongActivity.java
@@ -2,6 +2,8 @@ package com.example.a3d5bmusicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,6 +19,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.google.gson.JsonArray;
@@ -43,6 +46,13 @@ public class CurrentSongActivity extends AppCompatActivity {
     private TextView albumName;
     private TextView releaseYear;
     private ProgressBar progressBar;
+    private TextView currentTime;
+    private TextView wholeTime;
+
+    public static final String TAG = "MyTag";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +65,8 @@ public class CurrentSongActivity extends AppCompatActivity {
         albumName = findViewById(R.id.albumName);
         releaseYear = findViewById(R.id.releaseYear);
         progressBar = findViewById(R.id.songProgressBar2);
+        currentTime = findViewById(R.id.currentTime);
+        wholeTime = findViewById(R.id.wholeTime);
 
         msharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
         queue = Volley.newRequestQueue(this);
@@ -62,6 +74,14 @@ public class CurrentSongActivity extends AppCompatActivity {
         mHandler.postDelayed(runnable, 500);
 
 
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mHandler.removeMessages(0); 
     }
 
     private Handler mHandler = new Handler();
@@ -114,14 +134,34 @@ public class CurrentSongActivity extends AppCompatActivity {
                             String album = response.getJSONObject("item").getJSONObject("album").getString("name");
                             String releasedate = response.getJSONObject("item").getJSONObject("album").getString("release_date").substring(0,4);
                             String cover = response.getJSONObject("item").getJSONObject("album").getJSONArray("images").getJSONObject(2).getString("url");
-                            setMarquee(releaseYear,releasedate);
-                            setMarquee(albumName,album);
-                            setMarquee(songName,title);
-                            setMarquee(artistName,artist);
-                            Glide.with(CurrentSongActivity.this).load(cover).into(albumCover);
+                            setText_own(releaseYear,releasedate);
+                            setText_own(albumName,album);
+                            setText_own(songName,title);
+                            setText_own(artistName,artist);
+
+                            Glide.with(getApplicationContext()).load(cover).into(albumCover);
 
                             int current = response.getInt("progress_ms");
                             int whole = response.getJSONObject("item").getInt("duration_ms");
+                            int current_minute =(int)((float)current / 1000) / 60;
+                            int current_second = (int)((float)current /1000) % 60;
+                            String currenttime = "";
+                            if( current_second < 10){
+                                currenttime = current_minute + ":" +  "0" + current_second;
+                            }else{
+                                currenttime = current_minute + ":" + current_second;
+                            }
+
+                            setText_own(currentTime,currenttime);
+
+                            int whole_minute =(int)((float)whole / 1000) / 60;
+                            int whole_second = (int)((float)whole /1000) % 60;
+                            String wholetime = whole_minute + ":" + whole_second;
+                            setText_own(wholeTime,wholetime);
+
+
+
+
                             float progress = (float)current/whole;
                             progressBar.setProgress( (int)(100 * progress));
 
@@ -159,74 +199,16 @@ public class CurrentSongActivity extends AppCompatActivity {
 //                return params;
 //            }
         };
+
+        getRequest.setTag(TAG);
         queue.add(getRequest);
     }
 
-    /*private void getProgress() {
-        String endpoint = "https://api.spotify.com/v1/me/player/currently-playing?market=IE";
-        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, endpoint,null,
-                new Response.Listener<JSONObject>()
-                {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d("Response", response.toString());
-
-                        try {
-                            //String user_name = response.getString("display_name");
-                            //mUser.setText("Current User: " +user_name);
-
-                            int current = response.getInt("progress_ms");
-                            int whole = response.getJSONObject("item").getInt("duration_ms");
-                            float progress = (float)current/whole;
-                            progressBar.setProgress( (int)(100 * progress));
 
 
-
-
-                        }catch (JSONException e){
-                            Log.d("ERROR","error => "+e.toString());
-                        }
-
-                        //Log.d("Response", response.substring(1000, 2000));
-                        //Log.d("Response", response.substring(2000, 3000));
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("ERROR","error => "+error.toString());
-                    }
-                }
-        ) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String>  params = new HashMap<String, String>();
-                params.put("Authorization", "Bearer " + msharedPreferences.getString("token", ""));
-
-                return params;
-            }
-
-//            @Override
-//            protected Map<String, String> getParams() throws AuthFailureError {
-//                Map<String, String>  params = new HashMap<String, String>();
-//                params.put("q", "rock");
-//                params.put("type", "track");
-//
-//                return params;
-//            }
-        };
-        queue.add(getRequest);
-    }*/
-
-    private void setMarquee(TextView v, String text){
-
+    private void setText_own(TextView v, String text){
         v.setText(text);
-       //v.setSingleLine(true);
-        //v.setSelected(true);
-        //v.setFocusable(true);
-        //v.setFocusableInTouchMode(true);
-
     }
+
 
 }
