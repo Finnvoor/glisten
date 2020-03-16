4d0f1061d4adc57b2c37754a02c43a3f3a5da5ac
lil137 <scyyh2@nottingham.edu.cn>
2020-03-16 13:58:29 +0000

    Combine the login page into app

---
+++ app/src/main/java/com/example/a3d5bmusicapp/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.a3d5bmusicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button loginbubble;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.loginbutton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginbubble = findViewById(R.id.loginbubble);
+        loginbubble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
